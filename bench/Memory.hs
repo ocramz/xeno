e7d42c6ff274424498b4c98e26765b8a7e8315d0
Chris Done <chrisdone@gmail.com>
2016-12-30 15:31:42 +0000

    Update memory benchmarks to match speed

--- bench/Memory.hs
+++ bench/Memory.hs
@@ -9,13 +9,9 @@ import qualified Xeno
 main :: IO ()
 main = do
   f4kb <- S.readFile "data/books-4kb.xml"
-  f42kb <- S.readFile "data/ibm-oasis-42kb.xml"
-  f52kb <- S.readFile "data/oasis-52kb.xml"
+  f31kb <- S.readFile "data/text-31kb.xml"
+  f211kb <- S.readFile "data/fabricated-211kb.xml"
   mainWith
     (do func "4kb validate" Xeno.validate f4kb
-        func "42kb validate" Xeno.validate f42kb
-        func "52kb validate" Xeno.validate f52kb
-        -- func "4kb parseErikd" Xeno.parseErikd f4kb
-        -- func "42kb parseErikd" Xeno.parseErikd f42kb
-        -- func "52kb parseErikd" Xeno.parseErikd f52kb
-    )
+        func "31kb validate" Xeno.validate f31kb
+        func "211kb validate" Xeno.validate f211kb)
