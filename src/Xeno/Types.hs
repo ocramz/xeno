4ad28a1daa0580c8252cccc9b48e6e6ec805c025
Chris Done <chrisdone@gmail.com>
2017-01-11 15:30:56 +0000

    Make Exception NFData-able

--- src/Xeno/Types.hs
+++ src/Xeno/Types.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 -- | Shared types.
 
 module Xeno.Types where
 
+import Control.DeepSeq
 import Control.Exception
 import Data.Typeable
+import GHC.Generics
 
 data XenoException
   = XenoStringIndexProblem
   | XenoParseError
   | XenoExpectRootNode
-  deriving (Show, Typeable)
+  deriving (Show, Typeable, NFData, Generic)
 instance Exception XenoException where displayException = show
