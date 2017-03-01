eeefb76fd3bb366aa0d897d10673a0ae10dcfdcf
ocramz <zocca.marco@gmail.com>
2017-03-01 15:59:32 +0100

    build smoke test

--- test/Main.hs
+++ test/Main.hs
@@ -9,37 +9,39 @@ import           Test.Hspec
 import           Xeno.SAX
 import           Xeno.DOM
 
-main :: IO ()
-main = hspec spec
+main = print "hello!"
 
-spec :: SpecWith ()
-spec =
-  describe
-    "hexml tests"
-    (do mapM_
-          (\(v, i) -> it (show i) (shouldBe (Xeno.SAX.validate i) v))
-          hexml_examples_sax
-        let doc =
-              parse
-                "<root><test id=\"1\" extra=\"2\" />\n<test id=\"2\" /><b><test id=\"3\" /></b><test id=\"4\" /><test /></root>"
-        it
-          "children test"
-          (shouldBe
-             (map name (children doc))
-             ["test", "test", "b", "test", "test"])
-        it
-          "attributes"
-          (shouldBe
-             (attributes (head (children doc)))
-             [("id", "1"), ("extra", "2")]))
+-- main :: IO ()
+-- main = hspec spec
 
-hexml_examples_sax :: [(Bool, ByteString)]
-hexml_examples_sax =
-    [(True, "<test id='bob'>here<extra/>there</test>")
-    ,(True, "<test /><close />")
-    ,(True, "<test /><!-- comment > --><close />")
-    ,(True, "<test id=\"bob value\" another-attr=\"test with <\">here </test> more text at the end<close />")
-    ,(True, "<test></more>") -- SAX doesn't care about tag balancing
-    ,(False, "<test")
-    ,(True, "<?xml version=\"1.1\"?>\n<greeting>Hello, world!</greeting>")
-    ]
+-- spec :: SpecWith ()
+-- spec =
+--   describe
+--     "hexml tests"
+--     (do mapM_
+--           (\(v, i) -> it (show i) (shouldBe (Xeno.SAX.validate i) v))
+--           hexml_examples_sax
+--         let doc =
+--               parse
+--                 "<root><test id=\"1\" extra=\"2\" />\n<test id=\"2\" /><b><test id=\"3\" /></b><test id=\"4\" /><test /></root>"
+--         it
+--           "children test"
+--           (shouldBe
+--              (map name (children doc))
+--              ["test", "test", "b", "test", "test"])
+--         it
+--           "attributes"
+--           (shouldBe
+--              (attributes (head (children doc)))
+--              [("id", "1"), ("extra", "2")]))
+
+-- hexml_examples_sax :: [(Bool, ByteString)]
+-- hexml_examples_sax =
+--     [(True, "<test id='bob'>here<extra/>there</test>")
+--     ,(True, "<test /><close />")
+--     ,(True, "<test /><!-- comment > --><close />")
+--     ,(True, "<test id=\"bob value\" another-attr=\"test with <\">here </test> more text at the end<close />")
+--     ,(True, "<test></more>") -- SAX doesn't care about tag balancing
+--     ,(False, "<test")
+--     ,(True, "<?xml version=\"1.1\"?>\n<greeting>Hello, world!</greeting>")
+--     ]
