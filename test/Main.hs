99115a8864b924824bb394309f5374775fd33930
ocramz <zocca.marco@gmail.com>
2017-03-01 16:20:05 +0100

    initial

--- test/Main.hs
+++ test/Main.hs
@@ -8,40 +8,49 @@ import           Data.ByteString (ByteString)
 import           Test.Hspec
 import           Xeno.SAX
 import           Xeno.DOM
+import           Xeno.Types
+-- import Control.Monad.Catch
 
-main = print "hello!"
+import Data.Either
 
--- main :: IO ()
--- main = hspec spec
+-- main = print "hello!"
 
--- spec :: SpecWith ()
--- spec =
---   describe
---     "hexml tests"
---     (do mapM_
---           (\(v, i) -> it (show i) (shouldBe (Xeno.SAX.validate i) v))
---           hexml_examples_sax
---         let doc =
---               parse
---                 "<root><test id=\"1\" extra=\"2\" />\n<test id=\"2\" /><b><test id=\"3\" /></b><test id=\"4\" /><test /></root>"
---         it
---           "children test"
---           (shouldBe
---              (map name (children doc))
---              ["test", "test", "b", "test", "test"])
---         it
---           "attributes"
---           (shouldBe
---              (attributes (head (children doc)))
---              [("id", "1"), ("extra", "2")]))
+main :: IO ()
+main = hspec spec
 
--- hexml_examples_sax :: [(Bool, ByteString)]
--- hexml_examples_sax =
---     [(True, "<test id='bob'>here<extra/>there</test>")
---     ,(True, "<test /><close />")
---     ,(True, "<test /><!-- comment > --><close />")
---     ,(True, "<test id=\"bob value\" another-attr=\"test with <\">here </test> more text at the end<close />")
---     ,(True, "<test></more>") -- SAX doesn't care about tag balancing
---     ,(False, "<test")
---     ,(True, "<?xml version=\"1.1\"?>\n<greeting>Hello, world!</greeting>")
---     ]
+spec :: SpecWith ()
+spec =
+  describe
+    "hexml tests"
+    (do mapM_
+          (\(v, i) -> it (show i) (shouldBe (Xeno.SAX.validate i) v))
+          hexml_examples_sax
+        let doc =
+              parse
+                "<root><test id=\"1\" extra=\"2\" />\n<test id=\"2\" /><b><test id=\"3\" /></b><test id=\"4\" /><test /></root>"
+        it
+          "children test"
+          (shouldBe
+             (map name (children $ derp doc))
+             ["test", "test", "b", "test", "test"])
+        it
+          "attributes"
+          (shouldBe
+             (attributes (head (children $ derp doc)))
+             [("id", "1"), ("extra", "2")]))
+
+hexml_examples_sax :: [(Bool, ByteString)]
+hexml_examples_sax =
+    [(True, "<test id='bob'>here<extra/>there</test>")
+    ,(True, "<test /><close />")
+    ,(True, "<test /><!-- comment > --><close />")
+    ,(True, "<test id=\"bob value\" another-attr=\"test with <\">here </test> more text at the end<close />")
+    ,(True, "<test></more>") -- SAX doesn't care about tag balancing
+    ,(False, "<test")
+    ,(True, "<?xml version=\"1.1\"?>\n<greeting>Hello, world!</greeting>")
+    ]
+
+
+-- | Horrible hack. Don't try this at home.
+derp :: Either Xeno.Types.XenoException c -> c
+derp = either (error. show) id
